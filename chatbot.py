e840e21efb3eb380cafd38079f51e51501aebf67
Bandy <pemisin.toyo@gmail.com>
2021-04-26 00:14:08 -0400

    wellbot knows your name now

--- chatbot.py
+++ chatbot.py
@@ -3,6 +3,7 @@ import json
 import pickle
 
 import numpy as np
+import spacy
 import nltk
 from nltk.stem import WordNetLemmatizer
 from speech_recog import recognize
@@ -41,6 +42,9 @@ def predict_label(sentence):
     result = model.predict(np.array([bag_of_words]))[0]
     results = [[i, j] for i, j in enumerate(result) if j > 0.25]
 
+    if len(results) == 0:
+        return ['unknown']
+
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for i in results:
@@ -68,6 +72,27 @@ def prompt_user(tag):
             return prompt['responses'][0]
 
 
+def extract_info(tag):
+    message = recognize()
+    print("User:", message)
+    # Clean the message and then use spacy for entity recognition
+    clean_msg = clean_sentences(message)
+
+    spacy_nlp = spacy.load('en_core_web_sm')
+    document = spacy_nlp((" ".join(clean_msg)).strip())
+    # Stores all the entities in the sentence such as names or places
+    entities = []
+    print("There are", len(document.ents), "entities")
+    print([(X.text, X.label_) for X in document.ents])
+    if tag == "user_name":
+        for i in document.ents:
+            if i.label_ in ["PERSON"]:
+                entities.append(i.text)
+                print("Here we are")
+                print(i)
+    return entities
+
+
 def start_bot():
     message = recognize()
     ints = predict_label(message)
@@ -80,9 +105,8 @@ def start_bot():
 
 if __name__ == '__main__':
     intro = prompt_user("welcome")
-
     print("Wellbot:", to_speech(intro))
+    name = prompt_user("user_name") + " " + extract_info("user_name")[0]
+    print("Wellbot:", to_speech(name))
     while True:
         start_bot()
-
-
